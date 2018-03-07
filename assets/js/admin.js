6af4fcff64284f75fe51ea49a14a937fe7d06f17
funtomi <396166153@qq.com>
2018-03-07 21:03:06 +0800

    Modify HomePage

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,10 +1,7 @@
 $(function() {
     $('#side-menu').metisMenu();
 });
-
-//Loads the correct sidebar on window load,
-//collapses the sidebar on window resize.
-// Sets the min-height of #page-wrapper to window size
+ 
 $(function() {
     $(window).bind("load resize", function() {
         var topOffset = 50;
@@ -40,3 +37,5 @@ $(function() {
         }
     }
 });
+ 
+
