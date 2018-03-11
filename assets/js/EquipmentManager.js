d1e92a72b6f8f4b6fdcddfac5b3e25c5fc8b0c3d
funtomi <396166153@qq.com>
2018-03-11 18:05:30 +0800

    设备修改界面

--- assets/js/EquipmentManager.js
+++ assets/js/EquipmentManager.js
@@ -24,33 +24,6 @@
         });
     };
 
-    function ModifyData(oTable02, nRow, isAdd) {
-        var thisUrl;
-        if (isAdd) {
-            thisUrl = ajaxUrl + "user/addUser";
-        } else {
-            thisUrl = ajaxUrl + "user/editUser";
-        }
-        var jqInputs = $('input', nRow);
-        var aData = oTable02.fnGetData(nRow);
-        $.ajax({
-            type: 'POST',
-            data: { "id": parseInt(jqInputs[0].value), "username": jqInputs[1].value, "gender": jqInputs[2].value },
-            dataType: "json",
-            url: thisUrl,
-            success: function (data) {
-                if (data.success) {
-                    saveRow(oTable02, nRow);
-                    return;
-                }
-                ShowError(data.msg);
-            },
-            error: function (data) {
-                ShowError("保存失败！请检查输入！");
-            }
-        });
-    }
-
     function DeleteData(oTable02, nRow) {
         var aData = oTable02.fnGetData(nRow);
         $.ajax({
@@ -60,8 +33,7 @@
             url: ajaxUrl + "user/deleteUser",
             success: function (data) {
                 if (data.success) {
-                    oTable02.fnDeleteRow(nRow);
-                    nEditing = null;
+                    oTable02.fnDeleteRow(nRow); 
                     return;
                 }
                 ShowError(data.msg);
@@ -70,113 +42,31 @@
                 ShowError("删除失败!请检查网络连接！");
             }
         });
-    }
-
-    function ShowError(message) {
-        $("#errorMsgDiv").text(message);
-    }
-
-    //恢复表格
-    function restoreRow(oTable02, nRow) {
-        var aData = oTable02.fnGetData(nRow);
-        var jqTds = $('>td', nRow);
-
-        for (var i = 0, iLen = jqTds.length ; i < iLen ; i++) {
-            oTable02.fnUpdate(aData[i], nRow, i, false);
-        }
-        oTable02.fnDraw();
-    };
-
-    //修改
-    function editRow(oTable02, nRow, isAdd) {
-        var aData = oTable02.fnGetData(nRow);
-        var jqTds = $('>td', nRow);
-        jqTds[0].innerHTML = '<input class="form-control" type="text" value="' + aData[0] + '">';
-        jqTds[1].innerHTML = '<input class="form-control" type="text" value="' + aData[1] + '">';
-        jqTds[2].innerHTML = '<input class="form-control" type="text" value="' + aData[2] + '">';
-        if (isAdd) {
-            jqTds[3].innerHTML = '<a class="add save" href="#">保存 </a><a class="delete" href="#"> 删除</a>';
-        } else {
-            jqTds[3].innerHTML = '<a class="edit save" href="#">保存 </a><a class="delete" href="#"> 删除</a>';
-        }
-    };
-
-    //保存
-    function saveRow(oTable02, nRow) {
-        var jqInputs = $('input', nRow);
-        oTable02.fnUpdate(jqInputs[0].value, nRow, 0, false);
-        oTable02.fnUpdate(jqInputs[1].value, nRow, 1, false);
-        oTable02.fnUpdate(jqInputs[2].value, nRow, 2, false);
-        oTable02.fnUpdate('<a class="edit" href="#">修改 </a><a class="delete" href="#"> 删除</a>', nRow, 3, false);
-        oTable02.fnDraw();
-        nEditing = null;
-    };
-
+    }  
 
     // 添加新增按钮
     var addRowLink = '<a href="#" id="addRow" class="btn btn btn-primary"  style="margin-right:20px;">新增</a>';
     $('#dataTables-example_filter').prepend(addRowLink);
-
-    var nEditing = null;
-
-    // 新增行
+ 
+    // 新增
     $('#addRow').click(function (e) {
-        e.preventDefault();
-
-        //如果正在编辑则不新增
-        if (nEditing !== null) {
-            return;
-        }
-
-        var aiNew = oTable02.fnAddData(['', '', '', '<a class="edit" href="">修改 </a><a class="delete" href=""> 删除</a>']);
-        var nRow = oTable02.fnGetNodes(aiNew[0]);
-        editRow(oTable02, nRow, true);
-        nEditing = nRow;
-
-        $(nRow).find('td:last-child').addClass('actions text-center');
+        window.location.href = "../pages/EquipmentModify.html"; 
     });
 
-    $(document).on("click", "#dataTables-example a.delete", function (e) {
-        e.preventDefault();
+    //删除
+    $(document).on("click", "#dataTables-example a.delete", function (e) { 
         var nRow = $(this).parents('tr')[0];
         DeleteData(oTable02, nRow);
     });
 
+    //修改
     $(document).on("click", "#dataTables-example a.edit", function (e) {
         e.preventDefault();
         var nRow = $(this).parents('tr')[0];
-
-        if (nEditing !== null && nEditing != nRow) {
-            restoreRow(oTable02, nEditing);
-            editRow(oTable02, nRow, false);
-            nEditing = nRow;
-        }
-        else if (nEditing == nRow && this.innerHTML == "保存 ") {
-            ModifyData(oTable02, nEditing, false);
-        }
-        else {
-            editRow(oTable02, nRow, false);
-            nEditing = nRow;
-        }
+        var aData = oTable02.fnGetData(nRow);
+        window.location.href = "../pages/EquipmentModify.html?id="+aData[0]; 
     });
-
-    $(document).on("click", "#dataTables-example a.add", function (e) {
-        e.preventDefault();
-        var nRow = $(this).parents('tr')[0];
-
-        if (nEditing !== null && nEditing != nRow) {
-            restoreRow(oTable02, nEditing);
-            editRow(oTable02, nRow, true);
-            nEditing = nRow;
-        }
-        else if (nEditing == nRow && this.innerHTML == "保存 ") {
-            ModifyData(oTable02, nEditing, true);
-        }
-        else {
-            editRow(oTable02, nRow, true);
-            nEditing = nRow;
-        }
-    });
+ 
 });
 
 
--- assets/js/EquipmentModify.js
+++ assets/js/EquipmentModify.js
@@ -1,7 +1,14 @@
-﻿$(function () {
+﻿//获取get参数
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+ 
+$(function () {
+    var id = GetQueryString("id");
     $('#mainForm').bootstrapValidator({
         message: '不合法输入！',
-        //excluded:[":hidden",":disabled",":not(visible)"] ,//bootstrapValidator的默认配置
         excluded: ':disabled',//关键配置，表示只对于禁用域不进行验证，其他的表单元素都要验证
 
         feedbackIcons: {
@@ -10,30 +17,55 @@
             validating: 'glyphicon glyphicon-refresh'
         },
         fields: {
-            username: {
-                message: '用户名不能为空',//默认提示信息
+            id: { 
+                validators: {
+                    notEmpty: {
+                        message: '设备编号必填不能为空'
+                    },
+                    digits: {
+                        message: '设备编号只能由数字组成。'
+                    },
+                }
+            },
+            portId: { 
                 validators: {
                     notEmpty: {
                         message: '用户名必填不能为空'
                     },
-                    stringLength: {
-                        min: 6,
-                        max: 30,
-                        message: '用户名长度不能小于6位或超过30位'
+                    digits: {
+                        message: '端口编号只能由数字组成。'
                     },
-                    regexp: {
-                        regexp: /^[a-zA-Z0-9_\.]+$/,
-                        message: '用户名只能由字母、数字、点和下划线组成。'
-                    },
-
                 }
-            }
+            },
+            name: {
+                message: '设备名称不能为空',//默认提示信息
+                validators: {
+                    notEmpty: {
+                        message: '设备名称必填不能为空'
+                    },
+                }
+            },
+            engineRoom: {
+                message: '所属机房不能为空',//默认提示信息
+                validators: {
+                    notEmpty: {
+                        message: '所属机房必填不能为空'
+                    },
+                }
+            },
+            cabinet: {
+                message: '所属机柜不能为空',//默认提示信息
+                validators: {
+                    notEmpty: {
+                        message: '所属机柜必填不能为空'
+                    },
+                }
+            },
+
 
         }
     }).on('error.form.bv', function (e) {
-        console.log('error');
 
-        // Active the panel element containing the first invalid element
         var $form = $(e.target),
             validator = $form.data('bootstrapValidator'),
             $invalidField = validator.getInvalidFields().eq(0),
